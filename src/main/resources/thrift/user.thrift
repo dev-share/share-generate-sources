77b8aacd1e95e82a705c0c5406d7e786acf3a6bc
Kervin <zy5211314521@outlook.com>
2018-01-16 11:56:55 +0800

    thrift

--- src/main/resources/thrift/user.thrift
+++ src/main/resources/thrift/user.thrift
@@ -7,7 +7,6 @@ struct EntUserBean {
     3:string loginType;
     4:string extend;
     5:string userid;
-
 }
 
 struct UserBean {
@@ -15,31 +14,20 @@ struct UserBean {
     2:string name;  
     3:string nickName;
     4:string extend;
-
 }
 
 service  IUserDao{
   list<UserBean> queryAllEntUser();
-
   i32 addUserBean(1:UserBean user);
-
   UserBean updateUserBean(1:UserBean user);
-
   UserBean queryUserBean(1:i64 userID);
-
   UserBean deleteUserBean(1:i64 userID);
-
 }
 
 service  IEntUserDao{
   list<EntUserBean> queryAllEntUser();
-
   i32 addEntUser(1:EntUserBean user);
-
   EntUserBean updateEntUserBean(1:EntUserBean user);
-
   EntUserBean queryEntUserBean(1:i64 userID);
-
   EntUserBean deleteEntUserBean(1:i64 userID);
-
 }
